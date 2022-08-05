593e0c9ec89a33635f4b6fcec12d7b6db80c27bb
Mohammad <aali361@gmail.com>
2022-08-06 01:07:40 +0430

    add getting single object and all objects

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello</h1>'
+fakeDatabase={
+    1:{'name':'Clear car'},
+    2:{'name':'Write blog'},
+    3:{'name':'Start stream'}
+}
+
+class Items(Resource):
+    def get(self):
+        return fakeDatabase
+
+class Item(Resource):
+    def get(self, pk):
+        return fakeDatabase[pk]
+
+api.add_resource(Items, '/')
+api.add_resource(Item, '/<int:pk>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
