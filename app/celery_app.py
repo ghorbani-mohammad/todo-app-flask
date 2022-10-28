a02f011ab9180af5108a389cc21babb32f9bd571
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-28 17:27:32 +0330

    minor change

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,21 +1,26 @@
 from .app import app
 from celery import Celery
 
+
 def make_celery(app):
     celery = Celery(app.import_name)
     celery.conf.update(app.config["CELERY_CONFIG"])
-    celery.conf.broker_url = 'redis://prepare_4_redis:6379'
-    celery.conf.result_backend = 'redis://prepare_4_redis:6379'
+    celery.conf.broker_url = "redis://prepare_4_redis:6379"
+    celery.conf.result_backend = "redis://prepare_4_redis:6379"
 
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return self.run(*args, **kwargs)
+
     celery.Task = ContextTask
     return celery
 
-app.config.update(CELERY_CONFIG={
-    'broker_url': 'redis://prepare_4_redis:6379',
-    'result_backend': 'redis://prepare_4_redis:6379',
-})
+
+app.config.update(
+    CELERY_CONFIG={
+        "broker_url": "redis://prepare_4_redis:6379",
+        "result_backend": "redis://prepare_4_redis:6379",
+    }
+)
 celery = make_celery(app)
